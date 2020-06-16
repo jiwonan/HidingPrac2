d227652b7ad954e92e03b39f19f488f44b125f80
Jiwon An <s2018s31@e-mirim.hs.kr>
2020-06-16 10:41:28 +0900

    초기 커밋.

---
+++ formabstract/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formabstract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용 블라블라");
+            MessageBox.Show("내용 블라블라", "제목", MessageBoxButtons.OKCancel);
+            MessageBox.Show("내용 블라블라", "제목",
+                MessageBoxButtons.AbortRetryIgnore,
+                MessageBoxIcon.Asterisk);
+        }
+    }
+}
